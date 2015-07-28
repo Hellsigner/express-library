f695d0cfaa67f22dff2a117f4a703e8d33e79a88
Dmitry <hellsigner@gmail.com>
2015-07-28 18:13:21 +0300

    Integrated SwaggerUI (/api/docs) with stub json

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,12 @@
  */
 
 var express = require('express');
+var swaggerTools = require('swagger-tools');
+var swaggerDoc = require('../swagger/swagger.json');
+
 var router = express.Router();
 
 // todo: bearer auth strategy for API
-// todo: swagger on /api/swagger
 
 router.route('/books')
     .get(function (req, res, next) {
@@ -37,6 +39,33 @@ router.route('/books/:id(\\d+)')
     .all(function(req, res, next) {
         res.end(405);
     });
+
+// swagger
+router.get('/api-docs', function(req, res) {
+    res.json(swaggerDoc);
+});
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    router.use(middleware.swaggerMetadata());
+
+    // Validate Swagger requests
+    router.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    router.use(middleware.swaggerRouter(
+        {
+            //swaggerUi: '/swagger.json',
+            // controllers: './controllers',
+            useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+        }
+    ));
+
+    // Serve the Swagger documents and Swagger UI
+    router.use(middleware.swaggerUi({
+        apiDocs: '/api/api-docs',
+        swaggerUi: '/docs'
+    }));
+});
 
 // 404 handler
 router.use(function(req, res, next) {
