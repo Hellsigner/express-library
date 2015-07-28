d0a9bc9c28976828ecf2056fa558918458942eca
Dmitry <hellsigner@gmail.com>
2015-07-28 10:51:35 +0300

    Password service is now using config

--- service/password.js
+++ service/password.js
@@ -2,11 +2,12 @@
  * Created by hellsigner on 27.07.15.
  */
 
+var config = require(__dirname + '/../config');
 var bcrypt = require('bcrypt');
 
 module.exports = {
     hash: function(pass) {
-        return bcrypt.hashSync(pass, '$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa');
+        return bcrypt.hashSync(pass, config.bcrypt_salt);   // todo: make it async
     },
     compare: function(pass, hash, next) {
         return bcrypt.compare(pass, hash, next)
